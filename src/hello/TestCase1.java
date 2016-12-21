7ebc1d70fef529a182f7f24e9c3e1b683e587e9e
Ken Gao <kengao@locals-MacBook-Pro.local>
2016-12-21 15:41:20 -0800

    change to Junit test

---
+++ src/hello/TestCase1.java
@@ -0,0 +1,14 @@
+package hello;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCase1 {
+
+	@Test
+	public void test() {
+		System.out.println("Just to practise with Jenkins! ");
+	}
+
+}
